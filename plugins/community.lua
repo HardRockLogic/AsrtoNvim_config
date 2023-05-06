de26e0935ae9836359f7f117b41aee5109653065
HardRockLogic <neiton5001@gmai.com>
2023-05-06 18:06:35 +0300

    configured catppuccin themepack into community.lua/changed stadart theme

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,24 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  "catppuccin",
+  opts = {
+    flavour = "macchiato",
+    transparent_background = false,
+    term_colors = false,
+    integrations = {
+      sandwitch = false,
+      noice = true,
+      mini = true,
+      leap = true,
+      markdown = true,
+      neotest = true,
+      cmp = true,
+      overseer = true,
+      lsp_trouble = true,
+      ts_rainbow2 = true,
+    },
+  },
 }
